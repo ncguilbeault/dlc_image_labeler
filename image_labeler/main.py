f0b7d7f0aacfa59a3c6efe8a458c4de8af22f836
Nicholas <ncguilbeault@gmail.com>
2022-09-12 17:49:40 -0400

    Fixed bug when saving frames with no labels

--- image_labeler/main.py
+++ image_labeler/main.py
@@ -541,10 +541,16 @@ class MainWindow(QMainWindow):
             else:
                 x_offset = 0
                 y_offset = 0
-            zero_pad_image_name = len(str(max(list(self.labeled_frames.keys()))))
             scorer = self.config['scorer']
             if not self.config['multianimalproject']:
                 bodyparts = self.config['bodyparts']
+                for frame_key in self.labeled_frames.keys():
+                    all_coords = np.array(list(self.labeled_frames[frame_key].values())).astype(float)
+                    if np.isnan(all_coords).all():
+                        del(self.labeled_frames[frame_key])
+                if len(self.labeled_frames.items()) == 0:
+                    return
+                zero_pad_image_name = len(str(max(list(self.labeled_frames.keys()))))
                 columns = pd.MultiIndex.from_product([[scorer], bodyparts, ["x", "y"],], names=["scorer", "bodyparts", "coords",],)
                 idx = pd.MultiIndex.from_tuples([("labeled-data", video_stem, f"img{str(labeled_frame_key).zfill(zero_pad_image_name)}.png") for labeled_frame_key in self.labeled_frames.keys()])
                 data = np.array([np.array([self.labeled_frames[frame_key][bodypart] - np.array([x_offset, y_offset]) for bodypart in bodyparts]).ravel() for frame_key in self.labeled_frames.keys()], dtype=float)
@@ -558,6 +564,12 @@ class MainWindow(QMainWindow):
             else:
                 bodyparts = self.config['multianimalbodyparts']
                 individuals = self.config['individuals']
+                for frame_key in self.labeled_frames.keys():
+                    all_coords = np.array([self.labeled_frames[frame_key][individual][bodypart] for individual, bodyparts in self.labeled_frames[frame_key].items() for bodypart in bodyparts]).astype(float)
+                    if np.isnan(all_coords).all():
+                        del(self.labeled_frames[frame_key])
+                if len(self.labeled_frames.items()) == 0:
+                    return
                 columns = pd.MultiIndex.from_product([[scorer], individuals, bodyparts, ["x", "y"],], names=["scorer", "individuals", "bodyparts", "coords",],)
                 idx = pd.MultiIndex.from_tuples([("labeled-data", video_stem, f"img{str(labeled_frame_key).zfill(zero_pad_image_name)}.png") for labeled_frame_key in self.labeled_frames.keys()])
                 data = np.array([np.array([self.labeled_frames[frame_key][individual][bodypart] - np.array([x_offset, y_offset]) for individual in individuals for bodypart in bodyparts]).ravel() for frame_key in self.labeled_frames.keys()], dtype=float)
